3b2f7f9e7f38b9ef98608ee58766927f66f5c20c
gerproko <101721169+GerProko@users.noreply.github.com>
2022-03-31 14:32:35 -0300

    exercise 12 started

--- Assets/Scripts/EJ10.cs
+++ Assets/Scripts/EJ10.cs
@@ -15,16 +15,27 @@ using UnityEngine;
 public class EJ10 : MonoBehaviour
 {
 
-    int HORA_APERTURA = 10;
-    int HORA_CIERRE = 18;
+    const int HORA_APERTURA = 10;
+    const int HORA_CIERRE = 18;
     public int hora;
     bool estaAbierto = false;
 
     void Start()
     {
-        if (hora >= 1 && hora < 9)
+        if (hora <= 0 || hora >= 25)
         {
-            Debug.Log
+            Debug.Log("La hora ingresada no es valida. (variable estaAbierto = " + estaAbierto + ")");
+        }
+        else if (hora > 18 || hora < 10)
+        {
+            estaAbierto = false;
+            Debug.Log("El estacionamiento esta cerrado.(variable estaAbierto = " + estaAbierto + ")");
+            
+        }
+        else if (hora >= 10 && hora <= 18)
+        {
+            estaAbierto = true;
+            Debug.Log("El estacionamiento esta abierto. (variable estaAbierto = " + estaAbierto + ")" );
         }
     }
 
--- Assets/Scripts/EJ11.cs
+++ Assets/Scripts/EJ11.cs
@@ -1,16 +1,36 @@
-﻿using System.Collections;
+﻿/*Realizá un programa que resuelva el siguiente problema:
+Deberá ingresarse por Inspector una fecha de compra (String en el siguiente formato:
+YYYYMMDD), un nombre de comprador, un nombre de producto y una cantidad y precio del
+producto comprado. Mostrá a modo de ticket, la información ingresada y el monto a pagar.
+Modelo de Ticket:
+Fecha de Compra: YYYYMMDD
+Nombre del Comprador: xxxxx xxxxx
+Producto solicitado: xxxxx
+Cantidad solicitada: xx
+Precio Unitario: $xxx
+Total a Pagar: $xxxxx*/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EJ11 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string fecha;
+    public string nombre;
+    public string producto;
+    public int cantidad;
+    public int precio;
+
     void Start()
     {
-        
+        Debug.Log("Fecha de Compra: " + fecha);
+        Debug.Log("Nombre del Comprador: " + nombre);
+        Debug.Log("Producto solicitado: " + producto);
+        Debug.Log("Cantidad solicitada: " + cantidad);
+        Debug.Log("Precio Unitario: $" + precio);
+        Debug.Log("Total a Pagar: $" + cantidad * precio);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
